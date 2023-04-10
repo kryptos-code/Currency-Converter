552da59170c5c940514a6137caf1aa2bcd12cd9c
Shoaib Rehman <kryptosshoaib@gmail.com>
2023-04-11 01:02:21 +0530

    Implemented input for the values from the customer in CLI

--- ExchangeExpert/src/com/currency/exchange/main/ChangeCurrency.java
+++ ExchangeExpert/src/com/currency/exchange/main/ChangeCurrency.java
@@ -1,10 +1,43 @@
 package com.currency.exchange.main;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class ChangeCurrency {
 
 	public static void main(String[] args) {
 		
-
+		// Fetch list of Currencies to show on Panel
+		String[] currencies = {"KWD", "BHD", "OMR", "JOD", "KYD", "CHF", "EUR", "USD", "CAD", "INR"};
+		List<String> supportedCurrencies = Arrays.asList(currencies);
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("##################### Welcome to Exchange Expert #####################");
+		
+		System.out.println("Select currency type for conversion:");
+		int counter = 0;
+		for(String symbol: supportedCurrencies) {
+			counter++;
+			System.out.println(counter+". "+symbol);
+		}
+		System.out.print("Your input: ");
+		int cur1 = scan.nextInt();
+		
+		System.out.println("Select currency type for conversion:");
+		counter = 0;
+		for(String symbol: supportedCurrencies) {
+			counter++;
+			System.out.println(counter+". "+symbol);
+		}
+		System.out.print("Your input: ");
+		int cur2 = scan.nextInt();
+		
+		System.out.print("Enter amount for conversion: ");
+		int amount = scan.nextInt();
+		
+		// Need to implement exception handling for null, zero or out of index error to proceed further
 	}
 
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 An application that allows you to convert any currency into your picked up currency.
 
 List of Currency Symbols
-[KWD, BHD, OMR, JOD, KYD, GBP, CHF, EUR, USD, CAD, INR]
+["KWD", "BHD", "OMR", "JOD", "KYD", "GBP", "CHF", "EUR", "USD", "CAD", "INR"]
