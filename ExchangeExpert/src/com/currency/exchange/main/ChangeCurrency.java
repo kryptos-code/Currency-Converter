e2b94c1bd41c3f9b1d636a75ee10a888a4010e42
Shoaib Rehman <kryptosshoaib@gmail.com>
2023-04-16 01:40:49 +0530

    Implemented API using HttpClient API, Code cleaning & Refactoring pending

--- ExchangeExpert/src/com/currency/exchange/main/ChangeCurrency.java
+++ ExchangeExpert/src/com/currency/exchange/main/ChangeCurrency.java
@@ -2,9 +2,14 @@ package com.currency.exchange.main;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.math.BigDecimal;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +17,10 @@ import java.util.Map;
 import java.util.Scanner;
 
 import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONMLParserConfiguration;
+import org.json.JSONObject;
 
 import com.currency.exchange.util.ParameterStringBuilder;
 
@@ -25,8 +34,8 @@ public class ChangeCurrency {
 		int counter = 0;
 		int cur1 = 0;
 		int cur2 = 0;
-		double amount = 0d;
-		double convertedAmount = 0d;
+		BigDecimal amount = new BigDecimal(0);
+		BigDecimal convertedAmount = new BigDecimal(0);
 		String curSymbol1 = null;
 		String curSymbol2 = null;
 		int listSize = supportedCurrencies.size();
@@ -54,25 +63,28 @@ public class ChangeCurrency {
 			
 			// Take amount to convert
 			System.out.print("\nEnter amount for conversion: ");
-			amount = scan.nextDouble();
+			amount = scan.nextBigDecimal();
 		}
 		System.out.println(cur1+" "+cur2+" "+amount);
 		curSymbol1 = supportedCurrencies.get(cur1-1);
 		curSymbol2 = supportedCurrencies.get(cur2-1);
-		
+		System.out.println(curSymbol1+" "+curSymbol2+" "+amount);
 		// Need to implement exception handling for null, zero or out of index error to proceed further
 		convertedAmount = getConvertedAmount(curSymbol1, curSymbol2, amount);
+		
+		System.out.println("The exchange amount is Rs."+convertedAmount);
 		
 	}
 	
 	// Method: taking input from the user
-	
+	/*
 	static double getConvertedAmount(String currencyOne, String currencyTwo, double amount) {
 		System.out.println("STAGE 1: Inside getConvertedAmount");
 		double convertedAmount=0d;
-		String apiUrl = "https://api.freecurrencyapi.com";
+		String apiUrl = "https://api.freecurrencyapi.com/v1/latest";
 		URL url = null;
 		HttpsURLConnection con = null;
+		
 		
 		try {
 			System.out.println("STAGE 2: Inside try block");
@@ -87,28 +99,35 @@ public class ChangeCurrency {
 			parameters.put("base_currency", currencyTwo);
 			
 			con.setDoOutput(true);
+			con.setRequestProperty("Content-Type", "application/json");
+			
 			DataOutputStream out = new DataOutputStream(con.getOutputStream());
 			out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
 			out.flush();
 			out.close();
 			System.out.println("STAGE 4: request sent");
-//			con.setRequestProperty("Content-Type", "application/json");
-//			con.setConnectTimeout(5000);
-//			con.setReadTimeout(5000);
+			String contentType = con.getHeaderField("Content-Type");
+			
+			System.out.println("STAGE 4.5: Content Type-"+contentType);
+			
+			con.setConnectTimeout(5000);
+			con.setReadTimeout(5000);
 			System.out.println("STAGE 5: timeout set");
 			int status = con.getResponseCode();
 			System.out.println("STAGE 6: Status:"+status);
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			System.out.println("STAGE 6.5: Input read");
 			String inputLine;
 			StringBuffer content = new StringBuffer();
 			while ((inputLine = in.readLine()) != null) {
 			    content.append(inputLine);
 			}
-			System.out.println("Output:"+ content.toString());
+			System.out.println("STAGE 7 Output:"+ content.toString());
 
 			in.close();
 			con.disconnect();
 			
+			System.out.println("STAGE 8: Connection closed successfully");
 		} catch (Exception ex) {
 			System.out.println("Exception occured: "+ex.getMessage());
 			con.disconnect();
@@ -117,6 +136,60 @@ public class ChangeCurrency {
 				con.disconnect();
 			}
 		}
+		return convertedAmount;
+	}*/
+
+	static BigDecimal getConvertedAmount(String currencyOne, String currencyTwo, BigDecimal balance) {
+		System.out.println("STAGE 1: Inside getConvertedAmount");
+		BigDecimal convertedAmount= new BigDecimal(0);
+		String apiUrl = "https://api.freecurrencyapi.com/v1/latest?apikey=DzE9j6baYbAttkoiGoWIvHO2DbetE62tY4elb93c&currencies="+currencyTwo;
+		
+		
+		try {
+			System.out.println("STAGE 2: Inside try block");
+			
+			/*
+			Map<String, String> parameters = new HashMap<>();
+			parameters.put("apikey", "DzE9j6baYbAttkoiGoWIvHO2DbetE62tY4elb93c");
+			parameters.put("currencies", currencyOne);
+			parameters.put("base_currency", currencyTwo);
+			*/
+			
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create(apiUrl))
+					//.headers("apikey", "DzE9j6baYbAttkoiGoWIvHO2DbetE62tY4elb93c", "currencies", currencyOne)//, "base_currency",currencyTwo)
+					.method("GET", HttpRequest.BodyPublishers.noBody())
+					.build();
+			
+			System.out.println("Request is: "+ request.toString());
+
+			HttpResponse<String> response = null;
+			
+			try {
+				response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+				
+				System.out.println(response.body());
+				
+				JSONObject jObject = new JSONObject(response.body());
+				
+				JSONObject data = jObject.getJSONObject("data"); // get data object
+				BigDecimal currecyRate = data.getBigDecimal("INR");
+				
+				System.out.println("Currency Rate:"+currecyRate);
+				
+				convertedAmount = currecyRate.multiply(balance);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(response.body());
+			System.out.println("STAGE 8: Connection closed successfully");
+		} catch (Exception ex) {
+			System.out.println("Exception occured: "+ex.getMessage());
+		} 
+		
 		return convertedAmount;
 	}
 
